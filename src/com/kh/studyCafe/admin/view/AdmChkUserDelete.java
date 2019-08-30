8f39de1477b8dcbd748e39f553ca0c9bd7967ec8
moxn <moxn1948@gmail.com>
2019-08-31 02:35:02 +0900

    StudyCafe view 패널 컨트롤 기능

---
+++ src/com/kh/studyCafe/admin/view/AdmChkUserDelete.java
@@ -0,0 +1,23 @@
+package com.kh.studyCafe.admin.view;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class AdmChkUserDelete extends JPanel implements ActionListener{
+	
+	public AdmChkUserDelete() {
+
+		this.setBounds(100, 100, 410, 192);
+		this.setBackground(Color.BLACK);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+	
+}
