fdb13edb9e13855288d7d951b052df395b7fbdfb
허현주 <@rr>
2019-09-02 18:25:51 +0900

    팝법전부랑 합해서 총7개

--- src/com/kh/studyCafe/admin/view/AdmUnSelectSeat.java
+++ src/com/kh/studyCafe/admin/view/AdmUnSelectSeat.java
@@ -11,7 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class AdmUnSelectSeat extends JPanel implements ActionListener{
-//좌석을 선택해주세요 팝업1번
+//좌석을 선택해주세요 팝업1번 
 	      
 	public AdmUnSelectSeat() {
 		
@@ -35,9 +35,10 @@ public class AdmUnSelectSeat extends JPanel implements ActionListener{
 		JButton closeBtn = new JButton("Close");
 		
 		closeBtn.setBounds(45,120,326,50);
-		closeBtn.setBackground(new Color(18,177,157));
+		closeBtn.setBackground(new Color(189, 177, 157));
 		closeBtn.setFont(new Font("맑은 고딕",Font.BOLD,18));
 		closeBtn.setForeground(Color.WHITE);
+		closeBtn.setBorderPainted(false);
 		
 		
 		//패널에 올리기
